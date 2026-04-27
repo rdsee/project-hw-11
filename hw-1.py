2a9039883be88c27c8233778169fa004d2167e7a
rdsee <rost.slobod@gmail.com>
2026-04-28 00:16:45 +0300

    hw1 finished

---
+++ hw-1.py
@@ -0,0 +1,14 @@
+myfile = "hw.txt"
+
+with open(myfile) as f:
+    result = f.read().split()
+
+long_words = []
+for i in result:
+    if len(i) >= 7:
+        long_words.append(i)
+print(long_words)
+
+with open("hw2.txt", "w") as f2:
+    for word in long_words:
+        f2.write(word + "\n")
