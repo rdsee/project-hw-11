642f782bb29628fdffa84de5d7fdeb102d1ecdc3
rdsee <rost.slobod@gmail.com>
2026-04-27 14:30:21 +0300

    files, txt + reading

--- practice.py
+++ practice.py
@@ -20,3 +20,22 @@
 # with open("hello_1.txt", "a") as test_file:
 #     test_file.write("\nhwwwwwwwwwwk")
 
+# with open("hello_1.txt", "r") as myfile:
+    #1
+    #print(test_read_file.read()) - вариант если файл маленький и мало читать.
+    #2
+    # result = myfile.readline()
+    # print(result)
+    # result2 = myfile.readline(5) - read by lines
+    # print(result2)
+    #3
+    # result = myfile.readlines() - вывод всего содержимого в массив
+    # print(result)
+    #4
+    # for line in myfile:
+    #     print(line, end="") построчно читаем циклом файл
+    #5
+    # line = myfile.readline()
+    # while line:
+    #     print(line, end="")
+    #     line = myfile.readline() тоже с циклом просто ридлайн функция
