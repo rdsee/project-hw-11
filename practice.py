c0e8edef476e0f3276bd880f2dd0deb6359cc727
rdsee <rost.slobod@gmail.com>
2026-04-27 14:06:27 +0300

    files, txt + redacting

--- practice.py
+++ practice.py
@@ -1,12 +1,22 @@
-#v1
-try:
-    my_file = open("my_file.txt", "w")
-    try:
-        my_file.write("hello world")
-    except Exception as e:
-        print(e)
-    finally:
-        my_file.close()
-except Exception as e2:
-    print(e2)
+# #v1
+# try:
+#     my_file = open("my_file.txt", "w")
+#     try:
+#         my_file.write("hello world")
+#     except Exception as e:
+#         print(e)
+#     finally:
+#         my_file.close()
+# except Exception as e2:
+#     print(e2)
+
+#v2
+# with open("hello_1.txt", "w") as test_file:
+#     test_file.write("hello worldddd")
+#
+# with open("hello_1.txt", "a") as test_file:
+#     test_file.write("\nhello worldik")
+#
+# with open("hello_1.txt", "a") as test_file:
+#     test_file.write("\nhwwwwwwwwwwk")
 
