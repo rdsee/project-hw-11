a30f3cbb619f8fee65fc5feed93a7e909118ce0f
rdsee <rost.slobod@gmail.com>
2026-04-28 00:37:46 +0300

    hw2 finished

--- hw-1.py
+++ hw-1.py
@@ -12,3 +12,13 @@ print(long_words)
 with open("hw2.txt", "w") as f2:
     for word in long_words:
         f2.write(word + "\n")
+
+#2
+with open(myfile) as f:
+    result = f.read().split()
+
+print(f"Words: {len(result)}")
+
+
+
+
